d0defee746999dda42fc248d18d42a9e01dc31e3
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2024-10-20 21:28:32 +0500

    Update ExpCards.tsx

--- components/ExpCards.tsx
+++ components/ExpCards.tsx
@@ -9,7 +9,6 @@ import {
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { MoveRight } from "lucide-react";
-
 const jobPositions = [
   {
     timeline: "July 2023 — Present",
